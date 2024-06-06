536dbb96faece0aeebdeba302f43a90b9e213d55
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2024-06-06 10:52:24 -0400

    favi

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -1,10 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-    respond_to :json
-  
-    private
-  
-    def respond_with(resource, _opts = {})
-      render json: resource
-    end
+  respond_to :json
+
+  before_action :configure_sign_up_params, only: [:create]
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: resource
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
-  
